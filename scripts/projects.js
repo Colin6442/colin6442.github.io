3048f8365c92235ca7229b59ec96268e6d5bd451
Colin S <colin6442@gmail.com>
2023-02-23 02:55:21 -0800

    more progress on project page fundamentals/style

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,4 +1,7 @@
 //Pass element and size in vh
+
+var prev = -1;
+
 function onProject (element, size) {
 	let pc = [];
 	let p = [];
@@ -8,27 +11,75 @@ function onProject (element, size) {
 	}
 	var change = element.style;
 	var current = element.id;
-	change.height = size + "vh";
+	var curIndex = pc.indexOf(current);
+	change.height = size;
 	change.backgroundColor = "#ffffff0f";
 
-	for(var i = 0; i < 10; i++){
-		if(pc[i] != current){
-			document.getElementById(pc[i]).style.height = "20vh";
-			document.getElementById(pc[i]).style.backgroundColor = "";
-		}else{
-			document.getElementById(p[i]).style.marginTop = "0";
-			if(i%2 == 0){
-				document.getElementById(pc[i+1]).style.height = size + "vh";
-				document.getElementById(pc[i+1]).style.backgroundColor = "";
-				document.getElementById(p[i+1]).style.marginTop = "100%";
-				i++;
-			}else if(i%2 == 1){
-				document.getElementById(pc[i-1]).style.height = size + "vh";
-				document.getElementById(pc[i-1]).style.backgroundColor = "";
-				document.getElementById(p[i-1]).style.marginTop = "100%";
-				// document.getElementById(p[i]).style.marginTop = "0";
+	let Left = false;
+	let Right = false;
+
+	if(curIndex%2 == 0){
+		Left = true;
+	}else{
+		Right = true;
+	}
+
+
+	if(curIndex != prev){
+	
+		for(var i = 0; i < 10; i++){
+			if(pc[i] != current){
+				document.getElementById(pc[i]).style.height = "20vh";
+				document.getElementById(pc[i]).style.backgroundColor = "";
+			}else{
+				document.getElementById(p[i]).style.marginTop = "0";
+				if(Left){
+					document.getElementById(pc[i+1]).style.height = size;
+					document.getElementById(pc[i+1]).style.backgroundColor = "";
+					document.getElementById(p[i+1]).style.marginTop = "100vh";
+					i++;
+				}
+				if(Right){
+					document.getElementById(pc[i-1]).style.height = size;
+					document.getElementById(pc[i-1]).style.backgroundColor = "";
+					document.getElementById(p[i-1]).style.marginTop = "100vh";
+
+				}
 			}
 		}
+
+		if(Left){
+			for(let element of document.getElementsByClassName("colLeft")){
+				element.style.width = "70%";
+			}
+			for(let element of document.getElementsByClassName("colRight")){
+				element.style.width = "30%";
+			}
+		}
+		if(Right){
+			for(let element of document.getElementsByClassName("colLeft")){
+				element.style.width = "30%";
+			}
+			for(let element of document.getElementsByClassName("colRight")){
+				element.style.width = "70%";
+			}
+			
+		}
+
+		prev = curIndex;
+		
+	}else{
+		for(var i = 0; i < 10; i++){
+			document.getElementById(pc[i]).style.height = "20vh";
+			document.getElementById(pc[i]).style.backgroundColor = "";
+		}
+		for(let element of document.getElementsByClassName("colLeft")){
+			element.style.width = "50%";
+		}
+		for(let element of document.getElementsByClassName("colRight")){
+			element.style.width = "50%";
+		}
+		prev = -1;
 	}
 
 }
